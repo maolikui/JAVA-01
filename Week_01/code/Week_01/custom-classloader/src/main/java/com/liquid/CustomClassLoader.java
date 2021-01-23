2587944c303d6c11a580cfca3efae9e6bf59a972
maolikui <maolikuigaozhu@163.com>
2021-01-23 12:51:13 +0800

    update

--- Week_01/code/Week_01/custom-classloader/src/main/java/com/liquid/CustomClassLoader.java
+++ Week_01/code/Week_01/custom-classloader/src/main/java/com/liquid/CustomClassLoader.java
@@ -1,8 +1,12 @@
 package com.liquid;
 
 import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.StrUtil;
+import lombok.Cleanup;
 import lombok.SneakyThrows;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -12,16 +16,15 @@ import java.lang.reflect.Method;
  * @author Liquid
  */
 public class CustomClassLoader extends ClassLoader {
-    private final String path;
+    private String classPath;
 
-    public CustomClassLoader(String path) {
-        this.path = path;
+    public CustomClassLoader(String classPath) {
+        this.classPath = classPath;
     }
 
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        byte[] bytes = null;
-        bytes = readBytes(name);
+        byte[] bytes = readBytes(name);
         return defineClass(name, bytes, 0, bytes.length);
     }
 
@@ -33,18 +36,36 @@ public class CustomClassLoader extends ClassLoader {
      */
     @SneakyThrows
     private byte[] readBytes(String name) {
-        String xlassFilePath = path + name + ".xlass";
-        byte[] bytes = FileUtil.readBytes(xlassFilePath);
-        for (int i = 0; i < bytes.length; i++) {
-            bytes[i] = (byte) (255 - bytes[i]);
+        if (StrUtil.isBlank(name)) {
+            throw new Exception("IllegalName: " + name);
         }
-        return bytes;
+        String path = (StrUtil.isBlank(classPath) ? "" : classPath) + name.replaceAll("\\.", "/");
+        String encryClassPath = path + ".xlass";
+        //优先使用类名加密的字节码文件
+        if (FileUtil.exist(encryClassPath)) {
+            @Cleanup
+            FileInputStream in = new FileInputStream(encryClassPath);
+            @Cleanup
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1];
+            int size = 0;
+            while ((size = in.read(buffer)) != -1) {
+                //逐个字节解析
+                buffer[0] = (byte) (255 - buffer[0]);
+                out.write(buffer, 0, size);
+            }
+            return out.toByteArray();
+        }
+        return FileUtil.readBytes(path + FileUtil.CLASS_EXT);
     }
 
     public static void main(String[] args) {
         CustomClassLoader loader = new CustomClassLoader("D:\\Documents\\project\\Week_01\\custom-classloader\\");
+        //路径为null 时代表类路径
+//        CustomClassLoader loader = new CustomClassLoader(null);
         try {
             Class<?> hello = loader.findClass("Hello");
+            System.out.println("Hello 类加载器：" + hello.getClassLoader());
             Object o = hello.newInstance();
             Method method = hello.getMethod("hello");
             method.invoke(o);
